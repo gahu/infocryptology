66a1078cc368e09eae6f950fedfbeb28b4f1c07b
gahu <bgg8988@naver.com>
2018-06-21 18:43:12 +0900

    signup 및 signup 연결

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var recipesRouter = require('./routes/recipes');
 var accountsRouter = require('./routes/accounts');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/recipes', recipesRouter);
 app.use('/accounts', accountsRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
