f47f85b856c1367163a6c8efb2f37a4d97fca70f
sherry92 <guysuh123@naver.com>
2018-06-24 04:30:51 +0900

    finish!

--- routes/contract.js
+++ routes/contract.js
@@ -1,61 +1,178 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(express.static("public"));
-router.get("/", function(req, res){
-    res.sendFile(__dirname + "/views/index.jade");
-})
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var abi = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getContract_etc",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint16"
+			},
+			{
+				"name": "",
+				"type": "uint16"
+			},
+			{
+				"name": "",
+				"type": "uint16"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getCreator",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getContract_date",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_Address",
+				"type": "string"
+			},
+			{
+				"name": "_ContractDate",
+				"type": "string"
+			},
+			{
+				"name": "_ExpireDate",
+				"type": "string"
+			}
+		],
+		"name": "insertContract_date",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_Payment",
+				"type": "uint16"
+			},
+			{
+				"name": "_DownPayment",
+				"type": "uint16"
+			},
+			{
+				"name": "_leaseFee",
+				"type": "uint16"
+			}
+		],
+		"name": "insertContract_etc",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getContract_name",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_LessorName",
+				"type": "string"
+			},
+			{
+				"name": "_HirerName",
+				"type": "string"
+			},
+			{
+				"name": "_BrokerName",
+				"type": "string"
+			}
+		],
+		"name": "insertContract_name",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+];
 
-/*
-var Web3 = require("web3");
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var abi = web3.eth.contract(
-  [
-    {"constant":false,
-    "inputs":[{"name":"fileHash","type":"string"}],
-    "name":"get",
-    "outputs":[{"name":"timestamp","type":"uint256"},{"name":"owner","type":"string"}],
-    "payable":false,"type":"function"
-    },
-    {"constant":false,
-    "inputs":[{"name":"owner","type":"string"},{"name":"fileHash","type":"string"}],
-    "name":"set",
-    "outputs":[],
-    "payable":false,"type":"function"
-    },
-    {"anonymous":false,
-    "inputs":[{"indexed":false,"name":"status","type":"bool"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"owner","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],
-    "name":"logFileAddedStatus","type":"event"
-  }]);
-
-var contractaddress = abi.at("0x57da6aee3eb32c7c9192d52e374f05a12d449ff0");
-*/
-router.get('/submit', function(req, res, next) {
-  var fileHash = req.query.hash;
-    var owner = req.query.owner;
-    contractaddress.set.sendTransaction(owner, fileHash, {
-        from: web3.eth.accounts[0],
-    }, function(error, transactionHash){
-        if (!error)
-        {
-            res.send(transactionHash);
-        }
-        else
-        {
-            res.send("Error");
-        }
-    })
+/* GET home page. */
+router.get('/contract', function(req, res, next) {
+  res.render('contract');
 });
 
-
 router.post('/contract', function(req,res,next){
+	HouseContract = web3.eth.contract(abi);
+	console.log('here');
   res.render('contract', {
-    LessorName : req.body.LessorName,
-    HirerName:req.body.HirerName,
-    BrokerName:req.body.BrokerName,
-    Payment:req.body.Payment,
-    DownPayment:req.body.DownPayment,
-    leaseFee: req.body.leaseFee});
+    LessorName : 'a',
+    HirerName: 'b',
+    BrokerName: 'c',
+    Payment: 'd',
+    DownPayment: 'e',
+    leaseFee: 'f'});
 });
 
 module.exports = router;
